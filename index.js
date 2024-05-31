b6bc99491dd9f7ddedffd2a80c7adda41c71aa58
NahomAl <nahomalem95@gmail.com>
2024-05-31 10:19:22 +0300

    chore: Remove unused solution-header and solution-footer templates, update copyright year in footer

--- index.js
+++ index.js
@@ -13,21 +13,18 @@ const port = 3000;
 //Hint: Google to find out how to get the current year using JS.
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static("public"))
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
-  //Step 2 - Make the generate name functionality work
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
+  var adjIndex = Math.floor(Math.random() * adj.length);
+  var nounIndex = Math.floor(Math.random() *noun.length);
+  var bandName = `${adj[adjIndex]} ${noun[nounIndex]}`;
+  console.log(bandName);
+  res.render("index.ejs", { bandName: bandName });
 });
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
@@ -5692,3 +5689,4 @@ const noun = [
   "zoot-suit",
   "zucchini",
 ];
+
